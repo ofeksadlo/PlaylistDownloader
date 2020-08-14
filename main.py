ffdc4d43720a9229eda8314493d28af75c5af97c
ofeksadlo <55595248+ofeksadlo@users.noreply.github.com>
2020-08-14 15:20:24 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,42 +4,45 @@ from selenium.webdriver.common.by import By
 import time, os
 
 
+#Playlist Downloader
 
-
+#We start off by getting the url of the playlist we'll download the songs from. 
 urlToDownload = input('Enter the playlist url you want to download: ')
 driver = webdriver.Chrome(executable_path=r'D:\chromedriver.exe')
 
 
 
 driver.get(urlToDownload)
-time.sleep(2)
-songCounter = driver.find_element(by=By.CSS_SELECTOR, value='#publisher-container > div > yt-formatted-string > span:nth-child(3)').text
+driver.implicitly_wait(30)
+#We give 30 seconds for the page to load in most cases it will take 2 seconds. But there are slow connections out there.
+#First we retrive the number of songs in this playlist. Later on we'll use this variable to loop through the playlist.
+#The convertion for songCounter is done because of the for loop.
+songCounter = int(driver.find_element(by=By.CSS_SELECTOR, value='#publisher-container > div > yt-formatted-string > span:nth-child(3)').text)
 clear = lambda: os.system('cls')
 clear()
-print(songCounter)
+#We clear the console because we want to show our progress for the client.
 
-
-urlToDownload = driver.current_url
-
-
-songCounter = int(songCounter)
 
 for i in range(songCounter):
     clear()
+    #The console needs to get clear every time so the status we print represent the real progress.
     print("Downloading " + str(i+1) +" / "+ str(songCounter) + " songs")
+    #Opening a new tab to download the song we currently on.
     driver.execute_script("window.open('');")
+    #Switching to the new tab opened
     driver.switch_to.window(driver.window_handles[-1])
     driver.get('https://ytmp3.cc/')
+    #Entering the url of the song we currently on and downloading it.
     inputBox = driver.find_element_by_id('input')
     inputBox.send_keys(urlToDownload)
     inputBox.submit()
-    time.sleep(3)
     driver.implicitly_wait(30)
     driver.find_element_by_link_text('Download').click()
-
+    #We give the song a chance to start downloading (even though it's almost instantly).
     time.sleep(2)
     driver.close()
-
+    #We go back to first tab to download the next music clip of the playlist.
     driver.switch_to.window(driver.window_handles[0])
+    #Clicking the next button of the youtube player.
     driver.find_element_by_css_selector('#movie_player > div.ytp-chrome-bottom > div.ytp-chrome-controls > div.ytp-left-controls > a.ytp-next-button.ytp-button').click()
     urlToDownload = driver.current_url
